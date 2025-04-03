a448b6992861ad4f02b41358104dda9fe3c14970
Hieu Dang <hieudangk27@gmail.com>
2025-04-03 10:29:13 -0400

    fix location for Ho Tay

--- components/locationmap.tsx
+++ components/locationmap.tsx
@@ -5,11 +5,12 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 const containerStyle = {
   width: '100%',
   height: '400px',
+
 };
 
 const defaultCenter = {
-  lat: 42.91895952548739,
-  lng: 74.51503441433378,
+  lat: 21.075343712285203,
+  lng: 105.81956315396978,
 };
 
 const MapComponent = () => {
